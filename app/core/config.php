e7709c38f739fae88442895ac6d61674e4ae6113
Nnishchal <nnishchal@algomau.ca>
2025-07-15 18:53:04 +0000

    Configured the database!

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,8 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
+define('DB_HOST',         '4qdvxc.h.filess.io');
+define('DB_USER',         'test_grabbedago'); 
 define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
+define('DB_DATABASE',     'test_grabbedago');
 define('DB_PORT',         '3305');
