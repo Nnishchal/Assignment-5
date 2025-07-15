3c92f5ea2cb30792787f306cff0e447c14ef561f
Nnishchal <nnishchal@algomau.ca>
2025-07-15 19:22:14 +0000

    made some changes the home and login views

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,9 @@
 <?php require_once 'app/views/templates/header.php' ?>
-<div class="container">
+<div class="container my-3">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Hey</h1>
+                <h1>Hello, User</h1>
                 <p class="lead"> <?= date("F jS, Y"); ?></p>
             </div>
         </div>
--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,29 +1,34 @@
-<?php require_once 'app/views/templates/headerPublic.php'?>
-<main role="main" class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>You are not logged in</h1>
-            </div>
-        </div>
-    </div>
+	<?php require_once 'app/views/templates/headerPublic.php'?>
+	<main role="main" class="container  container-center" style="display: flex; flex-direction: column; align-items: center; justify-content: center; min-height: 100vh;">
+			<div class="page-header" id="banner">
+					<div class="row">
+							<div class="col-lg-12">
+									<h1 class="text text-decoration-none"><span class="text-decoration-underline">Login ^_^</span></h1>
+							</div>
+					</div>
+			</div>
 
-<div class="row">
-    <div class="col-sm-auto">
-		<form action="/login/verify" method="post" >
-		<fieldset>
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input required type="text" class="form-control" name="username">
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input required type="password" class="form-control" name="password">
-			</div>
-            <br>
-		    <button type="submit" class="btn btn-primary">Login</button>
-		</fieldset>
-		</form> 
-	</div>
-</div>
-    <?php require_once 'app/views/templates/footer.php' ?>
+	<div class="row">
+			<div class="col-sm-auto">
+			<form action="/login/verify" method="post" >
+			<fieldset>
+				<br>
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input required type="text" class="form-control" name="username">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input required type="password" class="form-control" name="password">
+				</div>
+							<br>
+					<div class="d-flex justify-content-center align-items-center gap-3">
+						<button type="submit" class="btn btn-primary d-block">Login</button>
+						<a href="/create">Click for Signup</a>
+					</div>
+			</fieldset>
+			</form>
+			<br>
+		</div>
+	</div>	
+	<?php require_once 'app/views/templates/footer.php' ?>
