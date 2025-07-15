9103d709cea62fb0eaa6d52b0ce4f703d3bf22b1
Nnishchal <nnishchal@algomau.ca>
2025-07-15 19:45:39 +0000

    created a new function in the controller for user creation

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -5,4 +5,9 @@ class Create extends Controller {
     public function index() {		
 	    $this->view('create/index');
     }
+
+    public function signup() {
+      $user = $this->model('User');
+      $user->signup($_REQUEST['username'], $_REQUEST['password'], $_REQUEST['confirm-password']);
+    }
 }
